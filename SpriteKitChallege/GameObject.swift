cf2f22abe608abe463e4b1883ac593fdb965afb8
Joao <joaovitornaru@hotmail.com>
2017-06-25 21:48:53 -0300

    Game Object.

--- SpriteKitChallege/GameObject.swift
+++ SpriteKitChallege/GameObject.swift
@@ -7,8 +7,22 @@
 //
 
 import UIKit
-import SpriteKit
 
-class GameObject: SKSpriteNode {
 
+enum CharacterState : String {
+    case walk = "Walking"
+    case idle = "Idle"
+    case jump = "Jumping"
+    case dead = "Dead"
+    case hit = "Hitting"
+}
+
+protocol GameObject {
+    
+    var inverted : Bool {get set}
+    var onGround : Bool {get set}
+    var characterState : CharacterState {get set}
+    
+    func setupPhysicsBody(objectSize : CGSize)
+    
 }
