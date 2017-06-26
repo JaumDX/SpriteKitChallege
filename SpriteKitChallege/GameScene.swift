553841cc3b4f71589c3ecfd0504257c8ed5d7afb
Joao <joaovitornaru@hotmail.com>
2017-06-25 21:44:51 -0300

    Init da GameScene.

--- SpriteKitChallege/GameScene.swift
+++ SpriteKitChallege/GameScene.swift
@@ -7,10 +7,36 @@
 //
 
 import SpriteKit
-import GameplayKit
+
+
+
 
 class GameScene: SKScene {
     
+    
+    let gameLayer : GameLayer?
+    let backgroundLayer : BackgroundLayer?
+    let hudLayer : HudLayer?
+    
+    override func didMove(to view: SKView) {
+        
+        self.addChild(gameLayer!)
+        self.addChild(backgroundLayer!)
+        self.addChild(hudLayer!)
+        
+    }
+    
+    override init(size : CGSize) {
+        
+        gameLayer = GameLayer(size: size)
+        backgroundLayer = BackgroundLayer(size: size)
+        hudLayer = HudLayer(size: size)
+        super.init(size : size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     
     
